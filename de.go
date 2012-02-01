0e99d6271a515e82041a28710069e306511b6864
Michał Derkacz <michal@Lnet.pl>
2012-02-01 23:00:34 +0100

    Use matrix.Dense insted of []float64

--- de.go
+++ de.go
@@ -4,34 +4,68 @@
 package de
 
 import (
+	"github.com/ziutek/matrix"
 )
 
-type Range struct {
-	Min, Max float64
-}
-
 type abc struct {
-	a, b, c []float64
+	a, b, c *matrix.Dense
 }
 
-type entity struct {
-	x, u []float64
-	in   chan abc
-	out  chan float64
+type agent struct {
+	x   *matrix.Dense
+	in  chan abc     // to send three vectors for crossover
+	out chan float64 // to obtain actual cost value for this agent
+	p   *Params
 }
 
+func newAgent(min, width *matrix.Dense, p *Params) *agent {
+	a := new(agent)
+	a.x = matrix.DenseZero(min.Size())
+	a.in = make(chan abc, 1)
+	a.out = make(chan float64, 1)
+	a.p = p
+	// Place this agent in random place on the initial area
+	a.x.Rand(0, 1)
+	a.x.MulBy(width)
+	a.x.AddTo(min, 1)
+	return a
+}
+
+// Crossover loop
+func (a *agent) crLoop() {
+	p := a.p
+	u := matrix.DenseZero(a.x.Size())
+	for in := range a.in {
+		// crx contains three vectors from three random agents
+		u.Sub(in.b, in.c, p.f)
+	}
+}
+
+type Params struct {
+	CR   float64                     // crossover probability (default 0.9)
+	cost func(*matrix.Dense) float64 // function to minimize
+	f    float64                     // differential weight
+}
+
+// Minimizes cost function by evoluting the population of multiple agents
 type Minimizer struct {
-	p  []entity // population
-	f  float64  // mutation factor
-	cr float64  // crossover probability
-	n  int      // dimensionality of the problem
-
-	cost func([]float64) float64 // function to minimize
+	Params Params
+	p      []*agent // population of agents
 }
 
 // Creates new minimizer
-// ia - area for initial population
-// cr - crossover probability
-func NewMinimizer(ia []Range, cr float64) *Minimizer {
-	return nil
+// cost - function to minimize
+// n - number of entities in population
+// min, max - area for initial population
+func New(cost func(*matrix.Dense) float64, n int, min, max *matrix.Dense) *Minimizer {
+	m := new(Minimizer)
+	m.p = make([]*agent, n)
+	m.Params.CR = 0.9
+	m.Params.cost = cost
+	// Initialization of population
+	max.AddTo(min, -1) // calculate width of initial area
+	for i := range m.p {
+		m.p[i] = newAgent(min, max, &m.Params)
+	}
+	return m
 }
--- examples/op.go
+++ examples/op.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"github.com/ziutek/de"
 	"image"
 	"image/color"
 	"image/png"
@@ -12,21 +11,19 @@ import (
 
 const (
 	scale = 50 // pixels/unit
+	minX = -5
+	maxX = 5
+	minY = -4
+	maxY = 4
 )
 
 var (
-	area = []de.Range{{-4, 4}, {-4, 4}}
 	img  []byte
 )
 
 // This type describes the optimization problem for both: the optimizer and
 // the vizualization framework.
 type OP struct{}
-
-// Returns an initial area on which we search minimum of cost function
-func (o OP) Area() []de.Range {
-	return area
-}
 
 // Function to minimize
 func (o OP) Cost(x, y float64) float64 {
@@ -44,19 +41,17 @@ func (o OP) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func init() {
 	// Generate image that shows the cost function in the area
-	x0 := area[0].Min
-	y0 := area[1].Min
 	b := image.Rect(
 		0, 0,
-		int((area[0].Max-x0)*scale), int((area[1].Max-y0)*scale),
+		int((maxX-minX)*scale), int((maxY-minY)*scale),
 	)
 	m := image.NewNRGBA(b)
 	c := color.NRGBA{A: 255}
 	for y := 0; y < b.Max.Y; y++ {
 		for x := 0; x < b.Max.X; x++ {
 			f := ctx.OP.Cost(
-				x0+float64(x)/scale,
-				y0+float64(y)/scale,
+				minX+float64(x)/scale,
+				minY+float64(y)/scale,
 			)
 			if f < 0 {
 				c.G, c.B = 0, byte(-255*f)
