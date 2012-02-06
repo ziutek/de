79a08e764e1049e8ea24e94dd623d14eae580d46
Michał Derkacz <michal@Lnet.pl>
2012-02-06 02:15:40 +0100

    Working...

--- de.go
+++ de.go
@@ -5,6 +5,8 @@ package de
 
 import (
 	"github.com/ziutek/matrix"
+	"math/rand"
+	"time"
 )
 
 type abc struct {
@@ -12,32 +14,71 @@ type abc struct {
 }
 
 type agent struct {
-	x   *matrix.Dense
-	in  chan abc     // to send three vectors for crossover
-	out chan float64 // to obtain actual cost value for this agent
-	p   *Params
+	X      *matrix.Dense
+	in     chan abc     // to send three vectors for crossover
+	out    chan float64 // to obtain actual cost value for this agent
+	rnd    *rand.Rand
+	params *Params
 }
 
 func newAgent(min, width *matrix.Dense, p *Params) *agent {
 	a := new(agent)
-	a.x = matrix.DenseZero(min.Size())
+	a.X = matrix.DenseZero(min.Size()).Hvec() // we operate on vectors only
 	a.in = make(chan abc, 1)
 	a.out = make(chan float64, 1)
-	a.p = p
+	a.params = p
+	a.rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 	// Place this agent in random place on the initial area
-	a.x.Rand(0, 1)
-	a.x.MulBy(width)
-	a.x.AddTo(min, 1)
+	a.X.Rand(0, 1)
+	a.X.MulBy(width)
+	a.X.AddTo(min, 1)
 	return a
 }
 
+// Returns random cut points for crossover
+func (a *agent) cutpoints() (start, stop int) {
+	n := len(a.X.Elems())
+	l := 1
+	for l < n && a.rnd.Float64() < a.params.CR {
+		l++
+	}
+	start = a.rnd.Intn(n)
+	stop = (start + l) % n
+	return
+}
+
+func perturb(u *matrix.Dense, in abc, f float64, start, stop int) {
+	v := u.Vslice(start, stop)
+	v.Sub(in.b.Vslice(start, stop), in.c.Vslice(start, stop), f)
+	v.AddTo(in.a.Vslice(start, stop), 1)
+}
+
 // Crossover loop
-func (a *agent) crLoop() {
-	p := a.p
-	u := matrix.DenseZero(a.x.Size())
+func (a *agent) crossoverLoop() {
+	u := matrix.DenseZero(a.X.Size())
+	n := u.Rows()
+	p := a.params
+
 	for in := range a.in {
-		// crx contains three vectors from three random agents
-		u.Sub(in.b, in.c, p.f)
+		x := a.X
+		start, stop := a.cutpoints()
+		// Crossover:
+		//  u = a + f * (b + c) for elements between start and stop
+		//  u = x               for remaining elements
+		if start < stop {
+			u.Vslice(0, start).Copy(x.Vslice(0, start))
+			perturb(u, in, p.f, start, stop)
+			u.Vslice(stop, n).Copy(x.Vslice(stop, n))
+		} else {
+			perturb(u, in, p.f, 0, stop)
+			u.Vslice(stop, start).Copy(x.Vslice(start, stop))
+			perturb(u, in, p.f, start, n)
+		}
+		// x = u if cost(u) <= cost(x)
+		/*if (... ) {
+			a.X = u
+			u = x
+		}*/
 	}
 }
 
@@ -49,8 +90,10 @@ type Params struct {
 
 // Minimizes cost function by evoluting the population of multiple agents
 type Minimizer struct {
-	Params Params
-	p      []*agent // population of agents
+	Params     Params
+	cols, rows int
+	p          []*agent // population of agents
+	best       *matrix.Dense
 }
 
 // Creates new minimizer
@@ -62,10 +105,16 @@ func New(cost func(*matrix.Dense) float64, n int, min, max *matrix.Dense) *Minim
 	m.p = make([]*agent, n)
 	m.Params.CR = 0.9
 	m.Params.cost = cost
+	m.cols, m.rows = min.Size()
 	// Initialization of population
 	max.AddTo(min, -1) // calculate width of initial area
 	for i := range m.p {
 		m.p[i] = newAgent(min, max, &m.Params)
 	}
 	return m
+}
+
+// Returns best resul
+func (m *Minimizer) Best() *matrix.Dense {
+	return matrix.NewDense(m.rows, m.cols, m.cols, m.best.Elems())
 }
